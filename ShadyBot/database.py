79232dffa0880a6648a3e34f9d807e1692bcecaf
Morris Choga <78124293+Mchoga@users.noreply.github.com>
2023-03-02 09:17:33 +0100

    Add files via upload

---
+++ ShadyBot/database.py
@@ -0,0 +1,5 @@
+songs_searched_results = {}
+albums_searched_results = {}
+album_downloaded_songs = []
+songs_root_location = ""
+track_num = 1
---
+++ ShadyBot/untitled1.py
@@ -0,0 +1,20 @@
+import requests
+
+import youtube_dl
+
+
+m = ["aa","bb","cc","dd","ee"]
+b = ['aa','bb','ff']
+
+
+
+for x in b:
+    if x in m:
+        m.remove(x)
+
+
+
+
+
+
+print (m)
