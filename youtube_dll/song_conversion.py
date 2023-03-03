f21723d0f9b890bd97a2c1394265e847ac5abd06
Mchoga <mchoga1036@gmail.com>
2023-03-03 23:29:41 +0100

    commit

--- youtube_dll/song_conversion.py
+++ youtube_dll/song_conversion.py
@@ -108,7 +108,6 @@ class conversion:
         mp4_no_frame.close()
 
         os.remove(vid_file)
-
         os.replace(audio_file, database.songs_root_location+"/"+yt.title+".mp3")
         # os.replace(audio_file, database.songs_root_location+"\\"+yt.title+".mp3")
 
