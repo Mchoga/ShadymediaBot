1ab26d6b6a02f9dc90a9dc11064f6b022a6bac81
Mchoga <mchoga1036@gmail.com>
2023-03-11 13:15:56 +0100

    commit

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def handle_message(update, context):
 
 
     elif bot_users[update.message.from_user.id] == 'album':
-        asyncio.create_task(conversion(update,context,application,"album").inLineKeyboardFeedback())
+        context.application.create_task(conversion(update,context,application,"album").inLineKeyboardFeedback())
 
     else:
         await update.message.reply_text("Invalid Command: Please choose a valid command")
